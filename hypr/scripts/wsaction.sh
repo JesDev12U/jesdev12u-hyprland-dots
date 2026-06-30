ac68995e0a70f225553f62ff08a572b82a1329ab
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2026-06-29 23:12:02 -0600

    feat(hypr): Problema con el cambio de escritorios 2

--- hypr/scripts/wsaction.sh
+++ hypr/scripts/wsaction.sh
@@ -19,10 +19,18 @@ active_ws=$(hyprctl activeworkspace -j | jq -r '.id')
 if [ "$group" -eq 1 ]; then
     # Move to group
     res=$(( (target_ws - 1) * 10 + active_ws % 10 ))
-    hyprctl dispatch "$dispatcher" "$res"
 else
     # Move to ws in group
     group_base=$(( (active_ws - 1) / 10 ))
     res=$(( group_base * 10 + target_ws ))
-    hyprctl dispatch "$dispatcher" "$res"
 fi
+
+if [ "$dispatcher" = "workspace" ]; then
+    lua_disp="hl.dsp.focus({ workspace = '$res' })"
+elif [ "$dispatcher" = "movetoworkspace" ]; then
+    lua_disp="hl.dsp.window.move({ workspace = '$res' })"
+else
+    lua_disp="hl.dsp.focus({ workspace = '$res' })"
+fi
+
+hyprctl dispatch "$lua_disp"
